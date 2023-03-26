44add883d7c0ed9f1a9defc0bf8acca8918358b0
SlavDog <llleosek@seznam.cz>
2023-03-26 21:28:10 +0200

    added plots, some changes

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from sklearn import linear_model
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 degrees = []
 preparation = []
 math_score = []
+our_input = []
 
 fhand = open("school-data-science-project\StudentsPerformance.csv")
 
@@ -22,14 +26,31 @@ def Prep_filter(item):
         return 0
     return 1
 
+def sigmoid(x, x0, k):
+     Y = 1 / (1 + np.exp(-k*(x-x0)))
+     return Y
+
 degrees_bool = list(map(Degrees_filter, degrees))
 prep_bool = list(map(Prep_filter, preparation))
-print(math_score)
-X = [degrees_bool,prep_bool]
-y = math_score
+
+Y = degrees_bool
+X = np.array([math_score]).reshape(-1,1)
+
+popt, pcov = curve_fit(sigmoid,math_score,Y)
+x1 = np.linspace(0, 100, 500)
+y1 = sigmoid(x1, *popt)
 
 logr = linear_model.LogisticRegression()
-logr.fit(X,y)
+logr.fit(X,Y)
+print(logr.score(X,Y))
+plt.plot(x1,y1)
+plt.scatter(X,Y)
+plt.xlabel("Math score")
+plt.ylabel("College")
+plt.show()
+
+predicted = logr.predict(np.array([40]).reshape(-1,1))
+print(f"{predicted}")
 
 
       
